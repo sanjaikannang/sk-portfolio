094c844a10829f33b2a3a3249306a7ff3b22f060
Sanjai Kannan G <136103561+sanjaikannang@users.noreply.github.com>
2025-04-26 12:31:45 +0530

    installed tailwind and created base folders and files

---
+++ src/Components/HeroSection/HeroSection.tsx
@@ -0,0 +1,11 @@
+const HeroSection = () => {
+    return (
+        <>
+            <div className="max-w-2xl md:mx-auto text-center lg:max-w-7xl mb-0 bg-lightGreen">
+                <h1 className="font-Agrandir text-3xl md:text-5xl lg:text-8xl mb-12"> Hi, I'm Sanjai Kannan G 👋</h1>
+            </div>
+        </>
+    )
+}
+
+export default HeroSection
